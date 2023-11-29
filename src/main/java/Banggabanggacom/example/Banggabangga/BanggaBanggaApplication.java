86fd386bdb4db3cfa0993bcb88b93138e0022a46
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-11-30 08:49:54 +0900

    initial commit

---
+++ src/main/java/Banggabanggacom/example/Banggabangga/BanggaBanggaApplication.java
@@ -0,0 +1,13 @@
+package Banggabanggacom.example.Banggabangga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanggaBanggaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanggaBanggaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Banggabanggacom/example/Banggabangga/BanggaBanggaApplicationTests.java
@@ -0,0 +1,13 @@
+package Banggabanggacom.example.Banggabangga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanggaBanggaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
