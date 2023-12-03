59a5e87d161e044c7101f1bdb7dafd3c0dfcda14
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-12-04 02:58:40 +0900

    feat() : 게시글, 댓글, 좋아요 도메인 생성

---
+++ src/main/java/Banggabanggacom/example/Banggabangga/domain/Post.java
@@ -0,0 +1,97 @@
+package Banggabanggacom.example.Banggabangga.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    private String content;
+
+    private int likeCount = 0;
+
+    private String writerNickname;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<Like> likes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments = new ArrayList<>();
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    protected Post() {
+    }
+
+    @Builder
+    private Post(String title, String content, User user, String writerNickname, List<Like> likes,
+                 List<Comment> comments) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.likes = likes;
+        this.writerNickname = writerNickname;
+        this.comments = comments;
+    }
+
+    public boolean isModified() {
+        return !createdAt.equals(modifiedAt);
+    }
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isOwner(Long accessUserId) {
+        if (accessUserId == null) {
+            return false;
+        }
+        return user.getId().equals(accessUserId);
+    }
+
+    public int getCommentCount() {
+        if (comments == null) {
+            return 0;
+        }
+        return comments.size();
+    }
+
+    public void addPostLike(Like like) {
+        likes.add(like);
+    }
+
+    public void deleteLike(Like like) {
+        likes.remove(like);
+        like.delete();
+    }
+
+}
